cd00e0e215f650aa232d07465bee39e98da4fd01
Johan Larsson <johan_gavle1@hotmail.com>
2015-11-08 20:50:23 +0100

    Refactor Keys.

--- Gu.Wpf.Validation/PropertyGrid/Keys.cs
+++ Gu.Wpf.Validation/PropertyGrid/Keys.cs
@@ -1,60 +1,51 @@
 ﻿namespace Gu.Wpf.Validation
 {
-    using System.Collections.Generic;
     using System.Runtime.CompilerServices;
     using System.Windows;
 
     public static class Keys
     {
-        private static readonly Dictionary<string, ComponentResourceKey> Cache = new Dictionary<string, ComponentResourceKey>();
+        public static ResourceKey SettingsListStyleKey { get; } = CreateKey();
 
-        public static ResourceKey SettingsListStyleKey { get { return Get(); } }
+        public static ResourceKey NestedListStyleKey { get; } = CreateKey();
 
-        public static ResourceKey NestedListStyleKey { get { return Get(); } }
+        public static ResourceKey HeaderStyleKey { get; } = CreateKey();
 
-        public static ResourceKey HeaderStyleKey { get { return Get(); } }
+        public static ResourceKey SettingStyleKey { get; } = CreateKey();
 
-        public static ResourceKey SettingStyleKey { get { return Get(); } }
-        
-        public static ResourceKey PlainTemplateKey { get { return Get(); } }
-        
-        public static ResourceKey SymbolTemplateKey { get { return Get(); } }
-        
-        public static object HelpTextAndSymbolTemplateKey { get { return Get(); } }
-        
-        public static object SymbolPathStyleKey { get { return Get(); } }
-        
-        public static object HelpErrorTextAndSymbolTemplateKey { get { return Get(); } }
-        
-        public static object HelpTemplateKey { get { return Get(); } }
+        public static ResourceKey PlainTemplateKey { get; } = CreateKey();
 
-        public static ResourceKey SettingStyleSelectorKey { get { return Get(); } }
+        public static ResourceKey SymbolTemplateKey { get; } = CreateKey();
 
-        public static ResourceKey ErrorSymbolTemplateSelectorKey { get { return Get(); } }
-        
-        public static ResourceKey CheckmarkGeometryKey { get { return Get(); } }
-        
-        public static ResourceKey ErrorGeometryKey { get { return Get(); } }
-        
-        public static ResourceKey RequiredGeometryKey { get { return Get(); } }
-        
-        public static ResourceKey QuestionGeometryKey { get { return Get(); } }
-        
-        public static object ErrorTextAndSymbolTemplateKey { get { return Get(); } }
+        public static object HelpTextAndSymbolTemplateKey { get; } = CreateKey();
 
-        public static object ErrorTemplateSelectorKey { get { return Get(); } }
-        
-        public static object ErrorTemplateKey { get { return Get(); } }
+        public static object SymbolPathStyleKey { get; } = CreateKey();
 
-        private static ComponentResourceKey Get([CallerMemberName] string caller = null)
+        public static object HelpErrorTextAndSymbolTemplateKey { get; } = CreateKey();
+
+        public static object HelpTemplateKey { get; } = CreateKey();
+
+        public static ResourceKey SettingStyleSelectorKey { get; } = CreateKey();
+
+        public static ResourceKey ErrorSymbolTemplateSelectorKey { get; } = CreateKey();
+
+        public static ResourceKey CheckmarkGeometryKey { get; } = CreateKey();
+
+        public static ResourceKey ErrorGeometryKey { get; } = CreateKey();
+
+        public static ResourceKey RequiredGeometryKey { get; } = CreateKey();
+
+        public static ResourceKey QuestionGeometryKey { get; } = CreateKey();
+
+        public static object ErrorTextAndSymbolTemplateKey { get; } = CreateKey();
+
+        public static object ErrorTemplateSelectorKey { get; } = CreateKey();
+
+        public static object ErrorTemplateKey { get; } = CreateKey();
+
+        private static ComponentResourceKey CreateKey([CallerMemberName] string caller = null)
         {
-            ComponentResourceKey key;
-            if (!Cache.TryGetValue(caller, out key))
-            {
-                key = new ComponentResourceKey(typeof(Keys), caller);
-                Cache.Add(caller, key);
-            }
-            return key;
+            return new ComponentResourceKey(typeof(Keys), caller); ;
         }
     }
 }
